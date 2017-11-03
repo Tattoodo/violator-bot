b95c3efc086e347b150a50f99720920f6f2247a2
Tobias Haagen Michaelsen <tobias.michaelsen@gmail.com>
2017-11-03 16:29:26 +0100

    Extract violation gathering

--- src/processPullRequest.js
+++ src/processPullRequest.js
@@ -1,6 +1,7 @@
 import GitHubApi from 'github';
 import eslintAdapter from './eslintAdapter';
 import stylelintAdapter from './stylelintAdapter';
+import flatMap from './flatMap';
 
 const config = {
   username: process.env.GITHUB_USERNAME,
@@ -65,32 +66,45 @@ const postReview = async (owner, repo, number, commit_id, comments) => {
   return github.pullRequests.createReview(review);
 };
 
+const collectViolations = async (owner, repo, number, commit_id) => {
+  const fetchContent = makeContentFetcher(owner, repo, commit_id);
+  const files = await getFiles(owner, repo, number);
+  const [eslintReviews, stylelintReviews] = await Promise.all([
+    eslintAdapter(fetchContent)(files),
+    stylelintAdapter(fetchContent)(files)
+  ]);
+
+  return [['ESLint', eslintReviews], ['stylelint', stylelintReviews]]
+    .reduce((acc, [title, reviews]) => {
+      if (reviews.length > 0) {
+        acc[title] = reviews;
+      }
+      return acc;
+    }, {});
+};
+
 const processPullRequest = async ({ owner, repo, number, commit_id }) => {
   const setStatus = createStatusSetter(owner, repo, commit_id);
   try {
     setStatus('pending', 'Linting…');
-    const fetchContent = makeContentFetcher(owner, repo, commit_id);
-    const files = await getFiles(owner, repo, number);
-    const [eslintReviews, stylelintReviews] = await Promise.all([
-      eslintAdapter(fetchContent)(files),
-      stylelintAdapter(fetchContent)(files)
-    ]);
-    const eslintViolations = eslintReviews.length > 0;
-    const stylelintViolations = stylelintReviews.length > 0;
-    const hasViolations = eslintViolations || stylelintViolations;
+
     let status = 'success';
     let message = 'No linting violations found';
-    if (hasViolations) {
+
+    const violations = await collectViolations(owner, repo, number, commit_id);
+    const keys = Object.keys(violations);
+    if (keys.length > 0) {
       status = 'failure';
-      message = `${[eslintViolations && `ESLint`, stylelintViolations && `stylelint`].filter(s => s).join(` and `)} violations found`;
+      message = `${keys.join(` and `)} violations found`;
       await postReview(
         owner,
         repo,
         number,
         commit_id,
-        eslintReviews.concat(stylelintReviews)
+        flatMap(keys.map(key => violations[key]))
       );
     }
+
     setStatus(status, message);
   } catch (error) {
     console.error('=== something bad happened!!!', error);
