4d0da5651d7052c5a0596d40a6f4693a432619fb
Tobias Haagen Michaelsen <tobias.michaelsen@gmail.com>
2018-10-09 15:33:28 +0200

    Remove superfluous development dependencies
    Latest version of rollup does not need all the Babel stuff

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,7 @@
-import babel from 'rollup-plugin-babel';
-
 export default {
   input: 'src/handler.js',
   output: {
     file: 'handler.js',
     format: 'cjs'
-  },
-  plugins: [
-    babel()
-  ]
+  }
 };
